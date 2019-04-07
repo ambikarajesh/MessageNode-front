f8f0ac61145b84010e52a46c94a6f0800a7dd7bc
ambikarajesh <ambikula@gmail.com>
2019-04-06 21:38:58 -0700

    lined signup and loginn pages using JWT authentication

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -52,7 +52,11 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8080/feed/posts')
+    fetch('http://localhost:8080/feed/posts?page='+page, {
+      headers:{
+        Authorization :'Bearer ' + this.props.token
+      }
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch posts.');
@@ -125,7 +129,10 @@ class Feed extends Component {
 
     fetch(url, {
       method:method,
-      body:formData
+      body:formData,
+      headers:{
+        Authorization :'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
@@ -178,7 +185,10 @@ class Feed extends Component {
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
     fetch('http://localhost:8080/feed/post/'+postId, {
-      method:'DELETE'
+      method:'DELETE',
+      headers:{
+        Authorization :'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
@@ -258,7 +268,7 @@ class Feed extends Component {
                 <Post
                   key={post._id}
                   id={post._id}
-                  author={post.creator.name}
+                  author={post.creator}
                   date={new Date(post.createdAt).toLocaleDateString('en-US')}
                   title={post.title}
                   image={post.imageUrl}
